91a16c69c6d1f91afe8cbde1be6d6f87182e17d7
lis017 <minhyeok12.dev@gmail.com>
2024-09-10 19:05:44 +0900

    내 fork에 일단 push하고, 그걸 pull request? 로 원본 레퍼지토리에 보내는거인듯.
    브랜치 생성을 위한 commit

---
+++ src/main/java/com/chatai/chataiweb/ChatAiWebApplication.java
@@ -0,0 +1,13 @@
+package com.chatai.chataiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAiWebApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/chatai/chataiweb/chat/ChatController.java
@@ -0,0 +1,25 @@
+package com.chatai.chataiweb.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final APIService apiService;
+
+    // 메인 페이지 (ChatAI 선택 화면)
+    @GetMapping("/")
+    public String main() {
+        apiService.callPythonApi(); // api 테스트
+        return "main.html";
+    }
+
+    // ChatAI 대화 화면
+    @GetMapping("/chat")
+    public String chat() {
+        return "chat.html";
+    }
+}
